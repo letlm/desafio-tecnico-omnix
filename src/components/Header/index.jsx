aeea0fa49cc4dc1f092fbd79555330dee4f98236
Letícia Leal Moreira <lealmleticia@gmail.com>
2022-10-10 23:59:19 -0300

    feature: footer insertion

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,16 @@
 function Header() {
   return (
-    <header className="flex flex-col w-full h-full items-center bg-blue500">
+    <header className="flex flex-col w-full h-24 items-center bg-blue500">
       <section className="flex items-center  w-10/12 h-24 max-w-[1100px]">
-        <span className="flex w-1/4 text-center text-white">LOGO </span>
+        <img
+          className="flex w-1/4 h-[50px] text-center text-white"
+          src="https://omnixdigital.com.br/logo-negative.svg"
+          alt="OMNIX"
+        />
         <div className="sm: hidden md:flex w-3/4 justify-end">
-          <button className="mr-10 text-white">Sign Up</button>
+          <button className="mr-10 text-white">Login</button>
           <button className="bg-blue400 text-white w-48 h-10 rounded-full cursor-pointer focus:outline-none">
-            Secondary
+            Sobre
           </button>
         </div>
       </section>
