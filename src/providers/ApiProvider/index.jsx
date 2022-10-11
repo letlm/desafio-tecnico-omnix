9d9a64bd68fbef57e3964b6011ae6e894497daec
Letícia Leal Moreira <lealmleticia@gmail.com>
2022-10-11 12:03:03 -0300

    feature cep treatment not found

--- src/providers/ApiProvider/index.jsx
+++ src/providers/ApiProvider/index.jsx
@@ -1,20 +1,26 @@
 import { createContext, useState } from "react";
 import { viaCEP } from "../../services";
 import { toast } from "react-toastify";
+import { useHistory } from "react-router-dom";
 
 export const ApiContext = createContext([]);
 
 function ApiProvider({ children }) {
   const [infos, setInfos] = useState([]);
+
+  const history = useHistory();
   const cepInfos = (cepRequest) => {
     viaCEP
       .get(`${cepRequest}/json/`)
-      .then((res) => setInfos(res.data))
-      .catch(() =>
-        toast("❌ Ocorreu um erro, por favor tente novamente!", {
-          className: "toastify-color-progress-error",
-        })
-      );
+      .then((res) => {
+        if (res.data.erro) {
+          toast.error("CEP não encontrado, por favor digitar um cep válido!");
+        } else {
+          setInfos(res.data);
+          history.push("/offers");
+        }
+      })
+      .catch(() => toast.error("Ocorreu um erro, por favor tente novamente!"));
   };
   return (
     <ApiContext.Provider
